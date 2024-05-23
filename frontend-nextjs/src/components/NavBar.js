be91d51a8c220b28a77996806a4bb145ce2abacb
Haziq Faiz <haziqfaiz.ripin@gmail.com>
2024-05-24 02:11:37 +0800

    navbar

---
+++ frontend-nextjs/src/components/NavBar.js
@@ -0,0 +1,112 @@
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+import resume from "../assets/pdf/Resume_Data_Science.pdf";
+import { BrowserRouter as Router } from "react-router-dom";
+
+export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  const onResumeButtonClick = () => {
+    const link = document.createElement("a");
+    link.href = resume;
+    link.download = "Haziq_Resume.pdf"; // specify the filename
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
+  return (
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                Projects
+              </Nav.Link>
+              <Nav.Link
+                href="#blog"
+                className={
+                  activeLink === "blog" ? "active navbar-link" : "navbar-link"
+                }
+              >
+                Blog
+              </Nav.Link>
+              <Button variant="outline-primary" onClick={onResumeButtonClick}>
+                Download My Resume
+              </Button>{" "}
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="#">
+                  <img src={navIcon1} alt="" />
+                </a>
+                <a href="#">
+                  <img src={navIcon2} alt="" />
+                </a>
+                <a href="#">
+                  <img src={navIcon3} alt="" />
+                </a>
+              </div>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  );
+};
